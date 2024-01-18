c69f587db0dadb2cccde0984c921ef2f372803f1
Fatiha Chehaib <155731772+fatiha098@users.noreply.github.com>
2024-01-18 22:34:12 +0100

    V4 main.py

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 from tkinter import *
-from tkinter import ttk
 from subprocess import call
 from PIL import Image, ImageTk
 
 app = Tk()
-app.title("Stock Management")
 app.geometry("800x500")
-
-
-icon_path = r"D:\ex\project\tkinter-Project\pin.png"
+app.title("Stock Management")
+icon_path = r"C:\Users\hp\Desktop\tkinter\projet\pin.png"
 img = Image.open(icon_path)
 icon = ImageTk.PhotoImage(img)
 app.iconphoto(True, icon)
@@ -17,33 +14,41 @@ app.iconphoto(True, icon)
 app.config(background="black")
 
 # background 
-background_image = PhotoImage(file = r"D:\ex\project\tkinter-Project\secondbg.png")
+background_image = PhotoImage(file = r"c:\Users\hp\Desktop\tkinter\projet\secondbg.png")
 background_label = Label(app, image=background_image)
 background_label.place(relwidth=1, relheight=1)
 
-def Purchase():
+def purchases():
   app.destroy()
-  call(["python", "purchase.py"])
+  call(["python", r"C:\Users\hp\Desktop\tkinter\projet\purchases.py"])
 
-def Sales():
+def sales():
   app.destroy()
-  call(["python", "sales.py"])
+  call(["python", r"C:\Users\hp\Desktop\tkinter\projet\sales.py"])
 
-
-head = Label(app, text="Here You Go To Manage Your Stock",font=("Arial", 20, "bold"),fg="black",bg="orange",pady=20,width=300)
-
-label1 =  Label(app, text="Please Choose one of the options :",font=("Arial", 16, "bold"),width="40",bg="black",fg="white")
-
-sales_btn = Button(app , text="Sales",width=25,font=("Arial", 17, "bold"), bg="orange",pady=13, command=Sales)
-
-purchase_btn = Button(app,text="Purchases",width=25, font=("Arial", 17, "bold"),pady=13, bg="orange",command=Purchase)
-
+head = Label(app, text="Here You Go To Manage Your Stock",
+              font=("Arial", 20, "bold"),
+              fg="black",
+              bg="orange",
+              pady=20,
+              width=300)
 head.place(relx=0.5, rely=0.08, anchor=CENTER)
-label1.place(relx=0.5,rely=0.3,anchor=CENTER)
+
+sales_btn = Button(app , text="Sales",width=25,
+                  font=("Arial", 17, "bold"), 
+                  bg="orange",
+                  pady=13,
+                  command=sales
+                  )
 sales_btn.place(relx=0.5,rely=0.5, anchor=E)
-purchase_btn.place(relx=0.5,rely=0.5, anchor=W)
+sales_btn.config(cursor="hand2")
 
-head.pack()
+purchases_btn = Button(app,text="Purchases",width=25, 
+                      font=("Arial", 17, "bold"),
+                      pady=13, 
+                      bg="orange",
+                      command=purchases)
+purchases_btn.place(relx=0.5,rely=0.5, anchor=W)
+purchases_btn.config(cursor="hand2")
 
-# Execution
 app.mainloop()
